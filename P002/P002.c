3c3081d8bded3c575e0c4765f2bbad8b7a987437
Mans Sterner <mans.sterner@gmail.com>
2017-01-21 13:01:58 +0100

    Solved P002

--- P002/P002.c
+++ P002/P002.c
@@ -2,9 +2,24 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#define LIMIT 4000000
 
 int main(int argc, char const *argv[])
 {
-  /* code */
+  int n1 = 1, n2 = 2;
+  int sum = 2, temp;
+
+  do {
+    temp = n1 + n2;
+    if ((temp % 2 == 0) && temp <= LIMIT) {
+      sum += temp;
+    }
+    n1 = n2;
+    n2 = temp;
+  } while(temp <= LIMIT);
+
+  printf("The sum of all even numbers in the Fibonacci sequence,"
+  " not exceeding four million, is: %d\n", sum);
+
   return 0;
 }
